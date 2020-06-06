463e4738a8c2eff5f9f39f21421a822761bab6f5
Czartor <czartoryskimateusz@gmail.com>
2020-06-06 11:25:56 +0200

    zmiany_ostateczne

--- cezar.py
+++ cezar.py
@@ -1,17 +1,17 @@
 print("Podaj ścieżkę do pliku:")
-path = input()
-file = open(path, "r")
+sciezka = input()
+file = open(sciezka, "r")
 data = file.read()
 file.close()
 new_data_as_list = []
 data_list = list(data)
 for d in data_list:
-    as_number = ord(d)+1
+    as_number = ord(d)-1
     as_string = chr(as_number)
     new_data_as_list.append(as_string)
     
 new_data = "".join(new_data_as_list)
-file = open(path, "w")
+file = open(sciezka, "w")
 file.write(new_data)
 file.close()
 
